8dec46dd72217e9cd6b6f546131964f294a9932d
Mariana <marianadealmeidapereira@outlook.com>
2021-02-27 13:35:14 -0300

    fifth day finished

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
@@ -7,12 +9,21 @@ class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    if(!(await schema.isValid(request.body))) {
+      throw new Error('Validation failed!');
+    }
+
     const usersRepository = getCustomRepository(UsersRepository)
 
     const userAlreadyExists = await usersRepository.findOne({ email });
 
     if(userAlreadyExists) {
-      response.status(400).send({ error: 'User already exists!' });
+      throw new AppError('User already exists!');
     }
 
     const user = usersRepository.create({
